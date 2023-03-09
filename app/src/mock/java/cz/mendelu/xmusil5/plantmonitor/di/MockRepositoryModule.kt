a6ab21368f8a6db5d4ccf10e9fd34a0cc5a61b12
Michal Musil <musilmichal16@gmail.com>
2023-03-09 17:58:00 +0100

    Added registration screen

--- app/src/mock/java/cz/mendelu/xmusil5/plantmonitor/di/MockRepositoryModule.kt
+++ app/src/mock/java/cz/mendelu/xmusil5/plantmonitor/di/MockRepositoryModule.kt
@@ -1,27 +1,23 @@
 package cz.mendelu.xmusil5.plantmonitor.di
 
-import cz.mendelu.xmusil5.plantmonitor.authentication.IAuthenticationManager
-import cz.mendelu.xmusil5.plantmonitor.communication.api.HousePlantMeasurementsApi
-import cz.mendelu.xmusil5.plantmonitor.communication.repositories.devices.DevicesRepositoryImpl
-import cz.mendelu.xmusil5.plantmonitor.communication.repositories.devices.DevicesRepositoryMock
-import cz.mendelu.xmusil5.plantmonitor.communication.repositories.devices.IDevicesRepository
-import cz.mendelu.xmusil5.plantmonitor.communication.repositories.measurements.IMeasurementsRepository
-import cz.mendelu.xmusil5.plantmonitor.communication.repositories.measurements.MeasurementsRepositoryImpl
-import cz.mendelu.xmusil5.plantmonitor.communication.repositories.measurements.MeasurementsRepositoryMock
-import cz.mendelu.xmusil5.plantmonitor.communication.repositories.plants.IPlantsRepository
-import cz.mendelu.xmusil5.plantmonitor.communication.repositories.plants.PlantsRepositoryImpl
-import cz.mendelu.xmusil5.plantmonitor.communication.repositories.plants.PlantsRepositoryMock
-import cz.mendelu.xmusil5.plantmonitor.communication.repositories.user_auth.IUserAuthRepository
-import cz.mendelu.xmusil5.plantmonitor.communication.repositories.user_auth.UserAuthRepositoryMock
+import cz.mendelu.xmusil5.plantmonitor.communication.api.repositories.devices.DevicesRepositoryMock
+import cz.mendelu.xmusil5.plantmonitor.communication.api.repositories.devices.IDevicesRepository
+import cz.mendelu.xmusil5.plantmonitor.communication.api.repositories.measurements.IMeasurementsRepository
+import cz.mendelu.xmusil5.plantmonitor.communication.api.repositories.measurements.MeasurementsRepositoryMock
+import cz.mendelu.xmusil5.plantmonitor.communication.api.repositories.plants.IPlantsRepository
+import cz.mendelu.xmusil5.plantmonitor.communication.api.repositories.plants.PlantsRepositoryMock
+import cz.mendelu.xmusil5.plantmonitor.communication.api.repositories.user_auth.IUserAuthRepository
+import cz.mendelu.xmusil5.plantmonitor.communication.api.repositories.user_auth.UserAuthRepositoryMock
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityRetainedComponent
 import dagger.hilt.android.scopes.ActivityRetainedScoped
 
+
 @Module
 @InstallIn(ActivityRetainedComponent::class)
-class MockRepositoryModule {
+class MockRepositoryModule2 {
 
     @ActivityRetainedScoped
     @Provides
